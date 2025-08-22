b381396b5d1d0ac506c0d17315b5813195b4f496
Mirzaliev Ruslan <ramirzaliev@gmail.com>
2025-08-22 13:46:50 +0300

    added Cli method getUserInput and invoking in App class

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,4 +1,16 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class Cli {
+    public static void getUserInput() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+    }
 }
