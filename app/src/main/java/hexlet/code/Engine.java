0bd01ed48684269d88225f0daa783fe726944590
Mirzaliev Ruslan w <ramirzaliev@gmail.com>
2025-09-03 17:31:01 +0300

    test Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,4 +1,23 @@
 package hexlet.code;
 
 public class Engine {
+
+    private String userName;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void greeting() {
+        System.out.print("Welcome to the Brain Games!\n\rMay I have your name? ");
+        //
+    }
+
+    public void finishMessage(boolean isSuccess) {
+        if (isSuccess) {
+            System.out.println("Congratulations, " + userName);
+        } else {
+            System.out.println("Let's try again, " + userName);
+        }
+    }
 }
