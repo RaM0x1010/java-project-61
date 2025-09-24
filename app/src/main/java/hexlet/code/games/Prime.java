4a70bffd99137fd67c5401cf83cdb768ae20330b
Ruslan Mirzaliev w <ramirzaliev@gmail.com>
2025-09-24 17:33:31 +0300

    change yes/no logic

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -6,31 +6,28 @@ import hexlet.code.Utils;
 public class Prime {
     private static final int MIN_NUMBER_OF_RANGE = 1;
     private static final int MAX_NUMBER_OF_RANGE = 100;
-    public static String isItPrimeNumber(int n) {
+
+    public static boolean isPrimeNumber(int n) {
         if (n < 2) {
-            return "no";
+            return false;
         }
         double sqrt = Math.sqrt(n);
         for (int i = 2; i <= sqrt; i++) {
             if (n % i == 0) {
-                return "no";
+                return false;
             }
         }
-        return "yes";
+        return true;
     }
+
     public static void playTheGame() {
         String ruleOfTheGame = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
         String[][] questionsAndAnswers =
                 new String[Engine.NUMBER_ROUND_DIMENSION][Engine.NUMBER_QA_DIMENSION];
         for (int i = 0; i < Engine.ROUNDS; i++) {
             int num = Utils.generateNumber(MIN_NUMBER_OF_RANGE, MAX_NUMBER_OF_RANGE);
-            for (int j = 0; j < questionsAndAnswers[i].length; j++) {
-                if (j == 0) {
-                    questionsAndAnswers[i][j] = String.valueOf(num);
-                } else {
-                    questionsAndAnswers[i][j] = isItPrimeNumber(num);
-                }
-            }
+            questionsAndAnswers[i][Engine.QUESTION_ARRAY_INDEX] = String.valueOf(num);
+            questionsAndAnswers[i][Engine.ANSWER_ARRAY_INDEX] = (isPrimeNumber(num)) ? "yes" : "no";
         }
         Engine.play(ruleOfTheGame, questionsAndAnswers);
     }
