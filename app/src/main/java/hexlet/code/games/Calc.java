cd03a7340a60f1b61bf9af92c041012a57210a3f
Ruslan Mirzaliev w <ramirzaliev@gmail.com>
2025-09-24 17:32:45 +0300

    create calculateExpression mathod

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -9,40 +9,34 @@ public class Calc {
     private static final int MIN_NUMBER_OF_RANGE = 1;
     private static final int MAX_NUMBER_OF_RANGE = 100;
 
+    public static int calculateExpression(int index, int firstNum, int secondNum) {
+        switch (index) {
+            case 0:
+                return firstNum - secondNum;
+            case 1:
+                return firstNum + secondNum;
+            case 2:
+                return firstNum * secondNum;
+            default:
+                System.out.println("Something is gone wrong!");
+                return 0;
+        }
+    }
+
     public static void playTheGame() {
         String ruleOfTheGame = "What is the result of the expression?";
-        int resultNum = 0;
         String[] operators = {"-", "+", "*"};
         String[][] questionsAndAnswers =
                 new String[Engine.NUMBER_ROUND_DIMENSION][Engine.NUMBER_QA_DIMENSION];
-
         for (int i = 0; i < Engine.ROUNDS; i++) {
             int operandOne = Utils.generateNumber(MIN_NUMBER_OF_RANGE, MAX_NUMBER_OF_RANGE);
             int operandTwo = Utils.generateNumber(MIN_NUMBER_OF_RANGE, MAX_NUMBER_OF_RANGE);
             int operationIndex  = Utils.generateNumber(
                     MIN_RANDOM_VALUE, MAX_RANDOM_VALUE) % MAX_RANDOM_VALUE;
-
-            switch (operationIndex) {
-                case 0:
-                    resultNum = operandOne - operandTwo;
-                    break;
-                case 1:
-                    resultNum = operandOne + operandTwo;
-                    break;
-                case 2:
-                    resultNum = operandOne * operandTwo;
-                    break;
-                default:
-                    System.out.println("Something is gone wrong!");
-            }
-
-            for (int j = 0; j < questionsAndAnswers[i].length; j++) {
-                if (j == 0) {
-                    questionsAndAnswers[i][j] = operandOne + " " + operators[operationIndex] + " " + operandTwo;
-                } else {
-                    questionsAndAnswers[i][j] = String.valueOf(resultNum);
-                }
-            }
+            questionsAndAnswers[i][Engine.QUESTION_ARRAY_INDEX] =
+                    operandOne + " " + operators[operationIndex] + " " + operandTwo;
+            questionsAndAnswers[i][Engine.ANSWER_ARRAY_INDEX] =
+                    String.valueOf(calculateExpression(operationIndex, operandOne,operandTwo));
         }
         Engine.play(ruleOfTheGame, questionsAndAnswers);
     }
