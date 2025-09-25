f3c4c681870826184b678f599f3ddc1b87085f6a
Ruslan Mirzaliev w <ramirzaliev@gmail.com>
2025-09-25 19:09:45 +0300

    remove unnecessary method. change generate progression.

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -12,48 +12,28 @@ public class Progression {
     private static final int MAX_START_NUM = 100;
     private static final int MIN_INDEX_MISSED_NUM = 0;
 
-    public static String outputProgression(int[] arrToStr, int indexOfMissedNumber) {
-
-        StringBuilder prepareStringProgression = new StringBuilder();
-
-        for (int i = 0; i < arrToStr.length; i++) {
-            if (i != indexOfMissedNumber) {
-                prepareStringProgression.append(arrToStr[i]);
-            } else {
-                prepareStringProgression.append("..");
-            }
-            if (i != arrToStr.length - 1) {
-                prepareStringProgression.append(" ");
-            }
+    public static String[] createProgression(int progressionLength) {
+        int startNumber = Utils.generateNumber(MIN_START_NUM, MAX_START_NUM);
+        int difference = Utils.generateNumber(MIN_LENGTH_DIFFERENCE, MAX_LENGTH_DIFFERENCE);
+        String[] progression = new String[progressionLength];
+        for (int i = 0; i < progression.length; i++) {
+            progression[i] = String.valueOf(startNumber + i * difference);
         }
-        return prepareStringProgression.toString();
-    }
-
-    public static int[] createProgression(int maxL, int startN, int diffStep) {
-        int[] progressionArray = new int[maxL];
-        for (int i = 0; i < progressionArray.length; i++) {
-            progressionArray[i] = startN + i * diffStep;
-        }
-        return progressionArray;
+        return progression;
     }
 
     public static void playTheGame() {
         String ruleOfTheGame = "What number is missing in the progression?";
         String[][] questionsAndAnswers =
                 new String[Engine.NUMBER_ROUND_DIMENSION][Engine.NUMBER_QA_DIMENSION];
+        int lengthProgression = Utils.generateNumber(MIN_LENGTH_PROGRESSION, MAX_LENGTH_PROGRESSION);
+        int missedNumber = Utils.generateNumber(MIN_INDEX_MISSED_NUM, lengthProgression - 1);
         for (int i = 0; i < Engine.ROUNDS; i++) {
-            int lengthProgression = Utils.generateNumber(MIN_LENGTH_PROGRESSION, MAX_LENGTH_PROGRESSION);
-            int startNumber = Utils.generateNumber(MIN_START_NUM, MAX_START_NUM);
-            int difference = Utils.generateNumber(MIN_LENGTH_DIFFERENCE, MAX_LENGTH_DIFFERENCE);
-            int missedNumber = Utils.generateNumber(MIN_INDEX_MISSED_NUM, lengthProgression - 1);
-            int[] progression = createProgression(lengthProgression, startNumber, difference);
-            for (int j = 0; j < questionsAndAnswers[i].length; j++) {
-                if (j == 0) {
-                    questionsAndAnswers[i][j] = outputProgression(progression, missedNumber);
-                } else {
-                    questionsAndAnswers[i][j] = String.valueOf(progression[missedNumber]);
-                }
-            }
+            String[] progression = createProgression(lengthProgression);
+            String answer = progression[missedNumber];
+            progression[missedNumber] = "..";
+            questionsAndAnswers[i][Engine.QUESTION_ARRAY_INDEX] = String.join(" ", progression);
+            questionsAndAnswers[i][Engine.ANSWER_ARRAY_INDEX] = answer;
         }
         Engine.play(ruleOfTheGame, questionsAndAnswers);
     }
